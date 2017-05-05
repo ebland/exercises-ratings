e2112d4d825ed623b430d74b16471b093ad0d559
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-04 17:55:13 -0700

    finished login up next logout

--- server.py
+++ server.py
@@ -51,18 +51,51 @@ def user_register_form():
 def user_register_process():
     """Process for user registration."""
 
-    email = User.email.query.all()
     email_received = request.form.get('email')
-    dup_email = []
+    email_check = User.query.filter_by(email=email_received).all()
 
-    for mail in email:
-        if mail == email_received:
-            dup_email.append(mail)
-        else:
-            email, password = Ratings.User.user_register_process(email)
-
+    if email_check == []:
+        password = request.form.get('password')
+        user=User(email=email_received, password=password)
+        db.session.add(user)
+        db.session.commit()
     return redirect("/")
 
+
+@app.route('/login', methods=['GET'])
+def user_login():
+
+    print session
+    return render_template("login_form.html")
+
+@app.route('/login', methods=['POST'])
+def verify_user_login():
+
+    user_email = request.form.get('user_email')
+    user_password = request.form.get('user_password')
+    response = User.query.filter_by(email=user_email).all()
+    if response == []:
+        flash('Wrong username and/or password. Try again or Create New Account')
+        return redirect("/register")
+    email = response[0].email
+    password = response[0].password
+   # import pdb; pdb.set_trace()
+    if email == user_email and password == user_password:
+        session['user_email'] = request.form['user_email']
+        flash('You were successfully logged in')
+        return redirect("/")
+    else:
+        flash('Wrong username and/or password. Try again or Create New Account')
+        return redirect("/register")
+@app.route('/logout', methods=['POST'])
+def user_logout():
+#     @app.route('/logout')
+# def logout():
+#    # remove the username from the session if it is there
+#    session.pop('username', None)
+#    return redirect(url_for('index'))
+
+    return render_template("logout_form.html")
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
@@ -70,6 +103,7 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
     connect_to_db(app)
+    db.session.commit()
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
